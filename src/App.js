0f737658d1d6ad06f099841e02ca9b7c91348510
Mateus Melo <mateus.melo@liferay.com>
2022-03-03 12:41:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./styles.css";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/films")
+      .then(({ data }) => {
+        setLoading(false);
+        setData(data);
+        setError(false);
+      })
+      .catch((err) => setError(true));
+  }, [data]);
+
+  const films = data?.results.map((f) => <li key={f.episode_id}>{f.title}</li>);
+
+  if (error) return <p>Error!</p>;
+
+  return <div>{!loading ? films : <p>Loading...</p>}</div>;
+}
+
+export default App;
